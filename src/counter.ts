90ccc1681c992edf1aa9af910ee4215fddfa8131
Anh <anhdt1911.work@gmail.com>
2024-07-31 02:01:54 +0700

    observable

--- src/counter.ts
+++ src/counter.ts
@@ -1,11 +1,55 @@
-import { count, fromEvent, map, scan, throttleTime } from "rxjs";
+import { count, fromEvent, map, Observable, scan, throttleTime } from "rxjs";
 
 export function setupCounter(element: HTMLButtonElement) {
-  fromEvent(document, "click")
-    .pipe(
-      throttleTime(1000),
-      map((event) => new MouseEvent(event.type, event).clientX),
-      scan((count, clientX) => count + clientX, 0)
-    )
-    .subscribe((count) => console.log(count));
+  // fromEvent(document, "click")
+  //   .pipe(
+  //     throttleTime(1000),
+  //     map((event) => new MouseEvent(event.type, event).clientX),
+  //     scan((count, clientX) => count + clientX, 0)
+  //   )
+  //   .subscribe((count) => console.log(count));
+  // const observable = new Observable((subscriber) => {
+  //   subscriber.next(1);
+  //   subscriber.next(2);
+  //   subscriber.next(3);
+  //   setTimeout(() => {
+  //     subscriber.next(4);
+  //     subscriber.complete();
+  //   }, 1000);
+  // });
+
+  // console.log("just before subscribe");
+  // observable.subscribe({
+  //   next(x) {
+  //     console.log("got value " + x);
+  //   },
+  //   error(err) {
+  //     console.error("something wrong occurred: " + err);
+  //   },
+  //   complete() {
+  //     console.log("done");
+  //   },
+  // });
+  // console.log("just after subscribe");
+  const observable = new Observable((subscriber) => {
+    const id = setInterval(() => {
+      subscriber.next("hi");
+    }, 1000);
+    return function unsubscribe() {
+      clearInterval(id);
+    };
+  });
+
+  const unsub = observable.subscribe((x) => console.log(x));
+  // unsub.unsubscribe();
+  setTimeout(() => {
+    console.log("unsubscribe");
+    unsub.unsubscribe();
+  }, 5000);
+  const unsub1 = observable.subscribe((x) => console.log(x));
+  // unsub.unsubscribe();
+  setTimeout(() => {
+    console.log("unsubscribe");
+    unsub1.unsubscribe();
+  }, 10000);
 }
